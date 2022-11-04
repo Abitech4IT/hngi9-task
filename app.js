3e37aa9db7e0ab5e454fe36c844ab7708ac6bea4
Abideen Abdulazeez <abitechit@gmail.com>
2022-11-04 17:32:20 +0100

    implementing a post req

--- app.js
+++ app.js
@@ -2,12 +2,31 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+app.get('/api/v1/hng-task', (req, res) => {
   res.status(200).json({
     slackUsername: 'Abitech',
     backend: true,
     age: 27,
     bio: 'Am a passionate and highly driven web developer 💻.',
+  });
+});
+
+app.post('/api/v1/hng-task', (req, res) => {
+  let x, y;
+  x = req.body.x;
+  y = req.body.y;
+
+  console.log(req.body);
+
+  let result = x + y;
+
+  res.status(201).json({
+    status: 'success',
+    slackUsername: 'Abitech',
+    operation_type: 'addition',
+    result,
   });
 });
 
